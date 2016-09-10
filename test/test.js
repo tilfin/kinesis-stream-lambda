63411798b5f8a60b5a9fc2c5db673faa94e20fde
Toshimitsu Takahashi <toshi@tilfin.com>
2016-09-11 00:20:44 +0900

    Up coverage, version 0.5

--- test/test.js
+++ test/test.js
@@ -42,13 +42,53 @@ describe('KLReadStream', () => {
           done();
         });
     });
+
+    const data3 = require('./fixtures/results/data-3');
+
+    it('read one record with expanding array', (done) => {
+      const event = require('./fixtures/events/data-3');
+      const readStream = KinesisLambda.reader(event);
+      const dataList = data3.concat();
+
+      readStream
+        .pipe(KinesisLambda.parseJSON({ expandArray: true }))
+        .pipe(es.map(function(data, callback) {
+          assert.deepEqual(data, dataList.shift());
+          callback(null, data)
+        }))
+        .on('end', function() {
+          assert.isOk(true);
+          done();
+        });
+    });
+
+    it('read one record with expanding array and counting by 2', (done) => {
+      const event = require('./fixtures/events/data-3');
+      const readStream = KinesisLambda.reader(event);
+      const dataList = data3.concat();
+
+      readStream
+        .pipe(KinesisLambda.parseJSON({ expandArray: true, countBy: 2 }))
+        .pipe(es.map(function(data, callback) {
+          const twoItems = dataList.splice(0, 2);
+          assert.deepEqual(data, twoItems);
+          callback(null, data)
+        }))
+        .on('end', function() {
+          assert.isOk(true);
+          done();
+        });
+    });
   });
 
   context('aggregation mode', () => {
+    const aggDate1 = require('./fixtures/results/agg-data-1');
+    const aggDate2 = require('./fixtures/results/agg-data-2');
+
     it('read one record', (done) => {
       const event = require('./fixtures/events/agg-data-1');
       const readStream = KinesisLambda.reader(event, { isAgg: true });
-      const dataList = require('./fixtures/results/agg-data-1');
+      const dataList = aggDate1.concat();
 
       readStream
         .pipe(KinesisLambda.parseJSON())
@@ -65,12 +105,48 @@ describe('KLReadStream', () => {
     it('read two records', (done) => {
       const event = require('./fixtures/events/agg-data-2');
       const readStream = KinesisLambda.reader(event, { isAgg: true });
-      const dataList = require('./fixtures/results/agg-data-2');
+      const dataList = aggDate2.concat();
 
       readStream
         .pipe(KinesisLambda.parseJSON())
         .pipe(es.map(function(data, callback) {
           assert.deepEqual(data, dataList.shift());
+          callback(null, data)
+        }))
+        .on('end', function() {
+          assert.isOk(true);
+          done();
+        });
+    });
+
+    it('read one records with counting by 3 items', (done) => {
+      const event = require('./fixtures/events/agg-data-1');
+      const readStream = KinesisLambda.reader(event, { isAgg: true });
+      const dataList = aggDate1.concat();
+
+      readStream
+        .pipe(KinesisLambda.parseJSON({ countBy: 3 }))
+        .pipe(es.map(function(data, callback) {
+          const threeItems = dataList.splice(0, 3);
+          assert.deepEqual(data, threeItems);
+          callback(null, data)
+        }))
+        .on('end', function() {
+          assert.isOk(true);
+          done();
+        });
+    });
+
+    it('read two records with counting by 4 items', (done) => {
+      const event = require('./fixtures/events/agg-data-2');
+      const readStream = KinesisLambda.reader(event, { isAgg: true });
+      const dataList = aggDate2.concat();
+
+      readStream
+        .pipe(KinesisLambda.parseJSON({ countBy: 4 }))
+        .pipe(es.map(function(data, callback) {
+          const threeItems = dataList.splice(0, 4);
+          assert.deepEqual(data, threeItems);
           callback(null, data)
         }))
         .on('end', function() {
