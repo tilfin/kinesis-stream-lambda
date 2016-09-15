8814f6e85d70b33f3169f5e4aee1b159c62c4f4c
Toshimitsu Takahashi <toshi@tilfin.com>
2016-09-16 01:44:55 +0900

    Add test

--- test/json_stream.test.js
+++ test/json_stream.test.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const fs = require('fs');
+const es = require('event-stream');
 const chai = require('chai');
 const assert = chai.assert;
 
@@ -22,6 +23,28 @@ describe('KinesisLambda.parseJSON', () => {
         });
 
       readStream.pipe(jsonStream);
+    });
+  });
+
+  context('passed 9 items JSON with count by 2 and expanding array', () => {
+    it('flush 5 items', (done) => {
+      const readStream = fs.createReadStream(__dirname + '/fixtures/data/data_json.txt');
+      const jsonStream = JSONTransform({ expandArray: true, countBy: 2 })
+        .on('error', function(err) {
+          assert.ifError(err);
+          done();
+        });
+
+      const writeStream = es.writeArray(function (err, array) {
+        assert.deepEqual(array[0], [{ id: 1 }, { id: 2 }]);
+        assert.deepEqual(array[1], [{ id: 3 }, { id: 4 }]);
+        assert.deepEqual(array[2], [{ id: 5 }, { id: 6 }]);
+        assert.deepEqual(array[3], [{ id: 7 }, { id: 8 }]);
+        assert.deepEqual(array[4], [{ id: 9 }]);
+        done();
+      });
+
+      readStream.pipe(jsonStream).pipe(writeStream);
     });
   });
 
